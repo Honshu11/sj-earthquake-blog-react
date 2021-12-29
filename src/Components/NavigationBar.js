56c133c1b2b54e14feab1ea66ef5cddbbdba577c
Honshu11 <egonza67@yahoo.com>
2021-12-28 16:08:06 -0800

    updated navbar bootstrap components. still no logo showing up.

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,15 +1,21 @@
+import { NavbarText } from 'bootstrap-react';
 import React from 'react';
+import {Navbar} from 'react-bootstrap';
 
 function NavigationBar() {
     return (
-        <div>
-            <nav className='navbar navbar-dark bg-dark'>
-               <a className='navbar-brand' href='#'>
-                   <img src='./Images/san-jose-quakes-logo.png' className='align-top' width="30" height="24" alt='san-jose-logo'></img>
-               </a> 
-            </nav>
+        <div className='Navbar-container'>
+            <Navbar bg="dark" variant="bg">
+                <Navbar.Brand href="#">
+                    <img 
+                    src='./Images/san-jose-quakes-logo.png'
+                    alt='San Jose Quakes'
+                    />
+                </Navbar.Brand>
+            </Navbar>
         </div>
     )
 }
 
 export default NavigationBar
+
